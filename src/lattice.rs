d108807916c8e3840e6b616d5b068cf947648349
RADIATOR-Bill <118129564+RADIATOR-Bill@users.noreply.github.com>
2024-06-07 19:31:36 +0100

    Streaming + Collision

--- src/lattice.rs
+++ src/lattice.rs
@@ -5,10 +5,10 @@ const WEIGHTS: [f32;Q] = [
     1./36., 1./9., 1./36.,
 ];
 
-const DIRECTIONS: [[i8;2];9] = [
-    [-1,1],  [0,1],  [1,1],
-    [-1,0],  [0,0],  [1,0],
-    [-1,-1], [0,-1], [1,-1],
+const DIRECTIONS: [[f32;2];9] = [
+    [-1.,1.],  [0.,1.],  [1.,1.],
+    [-1.,0.],  [0.,0.],  [1.,0.],
+    [-1.,-1.], [0.,-1.], [1.,-1.],
 ];
 
 enum Dir {}
@@ -87,32 +87,97 @@ impl Lattice {
         }
     }
 
-    pub fn density(&mut self) {
-        self.lattice.iter().for_each(|table| self.rho.add(table));
+    #[inline]
+    fn rotate_left (array: &mut [f32],shape: &D2) {
+        array.chunks_mut(shape.x).into_iter().for_each(|row| {
+            let last = row[0];
+            row.copy_within(1.., 0);
+            row[row.len() - 1] = last;
+        });
+    }
+    
+    #[inline]
+    fn rotate_right (array: &mut [f32],shape: &D2) {
+        array.chunks_mut(shape.x).into_iter().for_each(|row| {
+            let first = row[row.len() - 1];
+            row.copy_within(0..row.len() - 1, 1);
+            row[0] = first;
+        });
     }
 
-    pub fn velocities(&mut self) {
-        //ux//
-        [Dir::E,Dir::NE,Dir::SE].into_iter().for_each(|dir| self.ux.add(&self.lattice[dir]));
-        [Dir::W,Dir::NW,Dir::SW].into_iter().for_each(|dir| self.ux.sub(&self.lattice[dir]));
-        self.ux.div(&self.rho);
-        //uy//
-        [Dir::N,Dir::NE,Dir::NW].into_iter().for_each(|dir| self.uy.add(&self.lattice[dir]));
-        [Dir::S,Dir::SE,Dir::SW].into_iter().for_each(|dir| self.uy.sub(&self.lattice[dir]));
-        self.uy.div(&self.rho);
+    #[inline]
+    fn rotate_down (array: &mut [f32], shape: &D2) {
+        let (x,ln) = (shape.x,array.len());
+        let first = &array[ln - x..].to_vec();
+        array.copy_within(..ln - x, x);
+        array[..x].copy_from_slice(&first)
     }
 
-
+    #[inline]
+    fn rotate_up(array: &mut [f32], shape: &D2) {
+        let (x,ln) = (shape.x,array.len());
+        let last = &array[..x].to_vec();
+        array.copy_within(x.., 0);
+        array[ln - x..].copy_from_slice(&last);
+    }
 
     fn stream(&mut self) {
+        for (table, [x,y]) in &mut self.lattice[..Q-1].iter_mut().zip(DIRECTIONS) {
+            let (x,y) = (x as i8, y as i8);
+            match x {
+                1 => Lattice::rotate_right(&mut table.data, &table.dimensions),
+                -1 => Lattice::rotate_left(&mut table.data, &table.dimensions),
+                _ => (),
+            }
 
+            match y {
+                1 => Lattice::rotate_up(&mut table.data, &table.dimensions),
+                -1 => Lattice::rotate_down(&mut table.data, &table.dimensions),
+                _ => (),
+            }
+
+        }
     }
 
     fn collide(&mut self) {
-        self.density();
-        self.velocities();
-        todo!()
+        //density//
+        self.lattice.iter().for_each(|table| self.rho.add(table));
+
+        //velocities//
+            //ux//
+        [Dir::E,Dir::NE,Dir::SE].into_iter().for_each(|dir| self.ux.add(&self.lattice[dir]));
+        [Dir::W,Dir::NW,Dir::SW].into_iter().for_each(|dir| self.ux.sub(&self.lattice[dir]));
+        self.ux.div(&self.rho);
+            //uy//
+        [Dir::N,Dir::NE,Dir::NW].into_iter().for_each(|dir| self.uy.add(&self.lattice[dir]));
+        [Dir::S,Dir::SE,Dir::SW].into_iter().for_each(|dir| self.uy.sub(&self.lattice[dir]));
+        self.uy.div(&self.rho);
+
+        //collision//
+        {
+        use itertools::izip;
+        let omega = self.settings.omega;
+        for (table, weight, [x,y]) in izip!(self.lattice.iter_mut(),WEIGHTS.iter(),DIRECTIONS.iter()) {
+            if *x == 0. && *y == 0. {continue;}
+            for (cell, ux, uy) in izip!(table.data.iter_mut(), self.ux.data.iter(), self.uy.data.iter()) {
+                let magnitude = (ux * ux) + (uy * uy);
+                let dot = x * ux + y * uy;
+                *cell = omega * weight * (1. + 3. * dot + 4.5 * (dot * dot) - 1.5 * magnitude);
+            }
+        }
+        }
         
+        //Unit Velocity//
+        self.lattice[Dir::UNIT].add(&self.rho);
+        for i in (0..Q).filter(|&i| i != 4) {
+            let lhs = &self.lattice[i] as *const Table;
+            unsafe {self.lattice[Dir::UNIT].sub(&*lhs);}
+        }
+    }
+
+    pub fn simulate(&mut self) {
+        self.stream();
+        self.collide();
     }
 }
 
@@ -144,6 +209,7 @@ impl IndexMut<(usize,usize)> for Table {
     }
 }
 //Table Operations//
+#[allow(dead_code)]
 impl Table {
     fn add(&mut self, rhs: &Self) {
         self.data.iter_mut()
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,9 @@ mod lattice;
 use lattice::{Lattice, Settings,D3,Q};
 
 fn main() {  
-    let lbm = Lattice::new(&Settings {
-        dimensions: D3 {x: 100, y: 100, z: Q},
+    let mut lbm = Lattice::new(&Settings {
+        dimensions: D3 {x: 3, y: 3, z: Q},
         omega: 0.5,
     });
-    println!("{:?}", lbm[0][(1,2)]);
-    let mut a = [1.0, 2.0, 3.0, 4.0];
-    rotate_right(&mut a);
-    rotate_left(&mut a);
-    println!("{:?}", &mut a)
-
-}
-
-fn rotate_left (array: &mut [f32]) {
-    let last = array[0];
-    array.copy_within(1.., 0);
-    array[array.len() - 1] = last;
-}
-
-fn rotate_right (array: &mut [f32]) {
-    let first = array[array.len() - 1];
-    array.copy_within(0..array.len() - 1, 1);
-    array[0] = first;
+    lbm.simulate()
 }
